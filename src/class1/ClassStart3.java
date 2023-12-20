256af6484ce2a8f34f620079a4afdd5e340a3738
김소련 <kimsr9210@thekeytech.co.kr>
2023-12-20 15:50:05 +0900

    자바 프로젝트 생성

---
+++ src/class1/ClassStart3.java
@@ -0,0 +1,24 @@
+package class1;
+
+public class ClassStart3 {
+    public static void main(String[] args) {
+        //객체 도입
+        Student student1; //학생을 담을 수 있는 변수 선언
+
+        //new 실제 메모리에 학생과 관련된 데이터를 메모리에서 확보함 => 객체 또는 인스턴스라고 함
+        student1 = new Student(); //학생을 실제 메모리에 만들음
+        student1.name = "학생1";
+        student1.age = 15;
+        student1.grade = 90;
+
+        student1.age = 31;
+
+        Student student2 = new Student();
+        student2.name = "학생2";
+        student2.age = 16;
+        student2.grade = 80;
+
+        System.out.println("이름:" + student1.name+ "나이:" + student1.age + "성적 :" + student1.grade);
+        System.out.println("이름:" + student2.name+ "나이:" + student2.age + "성적 :" + student2.grade);
+    }
+}
---
+++ src/class1/Student.java
@@ -0,0 +1,11 @@
+package class1;
+
+public class Student {
+    //학생이라는 개념을 쓸수있음
+    //멤버변수, 필드라고 함
+    //클래스는 세가지 데이터를 가짐
+    String name;
+    int age;
+    int grade;
+
+}
