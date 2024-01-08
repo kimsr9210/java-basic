c34ef4eb083cde70a6bf09039b4315b3e35c4dfb
김소련 <kimsr9210@naver.com>
2024-01-08 17:31:26 +0900

    생성자 - 오버로딩과 this()

--- src/construct/ConstructMain2.java
+++ src/construct/ConstructMain2.java
@@ -1,6 +1,6 @@
 package construct;
 
-public class ConstructMain1 {
+public class ConstructMain2 {
     public static void main(String[] args) {
         //() 객체를 생성해라
         //그러면서 동시에 이 생성자를 불러라
@@ -8,8 +8,10 @@ public class ConstructMain1 {
         //new MemberConstruct() 객체생성
         //생성이 메모리에 만들어 지자 마자 호출 new MemberConstruct("user1", 15, 90);
         //객체를 생성하면서 메모리를 할당하고 생성자를 바로 부름
+        
+        //생성자 오버로딩
         MemberConstruct member1 = new MemberConstruct("user1", 15, 90); // Ctrl+p 파라미터 먼지 볼 수 있는 단축키
-        MemberConstruct member2 = new MemberConstruct("user2", 16, 80);
+        MemberConstruct member2 = new MemberConstruct("user2", 16);
 
         MemberConstruct[] members =  {member1, member2};
 
--- src/construct/MemberConstruct.java
+++ src/construct/MemberConstruct.java
@@ -4,6 +4,14 @@ public class MemberConstruct {
     String name;
     int age;
     int grade;
+
+    //생성자 하나 더 추가
+    MemberConstruct(String name, int age){ //생성자 오버로딩
+        //자기 자신의 생성자를 호출
+        //중복 제거
+        this(name, age, 50); //변경
+    }
+
     /*생성자
     생성자는 메서드와 비슷하지만 다음과 같은 차이가 있다.
     -생성자의 이름은 클래스 이름과 같아야 한다. 따라서 첫 글짜도 대문자로 시작
