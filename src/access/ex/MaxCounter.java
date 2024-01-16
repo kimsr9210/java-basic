0c17ba1d0ddfbcec6519e3a2d6c3f438b2364bb8
김소련 <kimsr9210@naver.com>
2024-01-16 10:05:07 +0900

    검증로직 추가

--- src/access/ex/MaxCounter.java
+++ src/access/ex/MaxCounter.java
@@ -8,11 +8,23 @@ public class MaxCounter {
         this.max = max;
     }
     public void increment(){
+        //코드가 명확하게 분리됨
+        //검증로직
         if(count >= max){
             System.out.println("최대값을 초과할 수 없습니다.");
             return;
         }
+        //실행로직
         count++;
+
+        /* 우리가 친숙한 방식
+       if(count >= max){
+            System.out.println("최대값을 초과할 수 없습니다.");
+            return;
+       }else{
+            count++;
+       }
+        * */
     }
 
     public int getCount(){
