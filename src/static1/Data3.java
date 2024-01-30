083368b1781789667643cf58fa1c6f272d742ab4
김소련 <kimsr9210@naver.com>
2024-01-30 17:34:19 +0900

    static 변수 접근

--- src/static1/Data3.java
+++ src/static1/Data3.java
@@ -3,7 +3,6 @@ package static1;
 public class Data3 {
     public String name;
     public static int count; //static //static이 붙은 변수는 메서드 영역에서 관리
-    public static int count1;
     public Data3(String name) {
         this.name = name;
         //Data3.count++;  내 클래스 안에서는 Data3. 생락가능
